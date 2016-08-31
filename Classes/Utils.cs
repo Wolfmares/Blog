86d2581b55b5198409ce37df928c68fcc75f9bef
Wolfmares <nataliyaboyanova@gmail.com>
2016-08-31 23:34:55 +0300

    Add files via upload

---
+++ Classes/Utils.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WolfmaresBlog.Classes
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLength=255)
+        {
+            if (text == null || text.Length <= maxLength)
+                return text;
+            var shortText = text.Substring(0, maxLength) + "...";
+            return shortText;
+        }
+    }
+}
